35e0587a9a2e68eb0da2f9490be9fee56f15c683
Minhaz Faisal <faisalahmed01944@gmail.com>
2025-06-14 14:57:09 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// js
+
+// toggle content
+function toggleV(text) {
+
+  const element = document.getElementById('text');
+
+  if (element.style.display == "block") {
+
+    element.style.display = "none"; 
+
+  } else {
+
+    element.style.display = "block";
+
+  }
+}
+
+
